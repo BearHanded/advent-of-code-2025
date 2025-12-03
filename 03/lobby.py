179996b36b2088a53346f454dac37b0ca2a41a28
BearHandedly <bcm3223@gmail.com>
2025-12-03 09:51:26 -0500

    day 3 pt 2, sliding window

--- 03/lobby.py
+++ 03/lobby.py
@@ -1,21 +1,31 @@
-from util import assert_equals, file_as_string, file_to_array
+from util import assert_equals, file_to_array
 
 TEST_INPUT = "test_input.txt"
 INPUT = "input.txt"
 
-def max_value(battery_array):
-    max_left_idx = battery_array[:-1].index(max(battery_array[:-1]))
-    max_right = max(battery_array[max_left_idx+1:])
-    return battery_array[max_left_idx] * 10 + max_right
+def max_value(battery_array, size):
+    start_idx = 0
+    total = 0
+    for i in range(size):
+        search_window = battery_array[start_idx:len(battery_array)-(size-i-1)]
+        max_left = max(search_window)
+        max_left_idx = search_window.index(max_left)
+        start_idx += max_left_idx + 1
+        total += 10**(size-i-1) * max_left
+    return total
 
-def sum_values(filename):
+def sum_values(filename, size):
     total = 0
     batteries = [list(int(i) for i in row) for row in file_to_array(filename)]
     for battery in batteries:
-        total += max_value(battery)
+        total += max_value(battery, size)
 
     print("Sum Values:", total)
     return total
 
-assert_equals(sum_values(TEST_INPUT), 357)
-sum_values(INPUT)
+
+assert_equals(sum_values(TEST_INPUT, 2), 357)
+sum_values(INPUT, 2)
+
+assert_equals(sum_values(TEST_INPUT, 12), 3121910778619)
+sum_values(INPUT, 12)
