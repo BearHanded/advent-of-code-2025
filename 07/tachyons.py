2225c67c0bd5bf558f9df8479f64bef654296816
BearHandedly <bcm3223@gmail.com>
2025-12-07 00:53:52 -0500

    day 7 yeehaw

--- 07/tachyons.py
+++ 07/tachyons.py
@@ -1,3 +1,4 @@
+from functools import cache
 from util import assert_equals, file_to_array
 
 TEST_INPUT = "test_input.txt"
@@ -31,6 +32,42 @@ def fire_beams(filename):
     print("Total collisions:", len(collisions))
     return len(collisions)
 
+
+timeline_grid = [[]]
+
+@cache
+def fire_beam_across_time(beam_y, beam_x):
+    if not (0 <= beam_x <= len(timeline_grid[0])):
+        return 0
+    total = 0
+    while True:
+        beam_y = beam_y + 1
+        if beam_y >= len(timeline_grid):
+            total += 1 # end of timeline
+            break
+
+        next_cell = timeline_grid[beam_y][beam_x]
+        if next_cell == ".":
+            continue
+        elif next_cell == "^":
+            total += fire_beam_across_time(beam_y, beam_x-1)
+            total += fire_beam_across_time(beam_y, beam_x+1)
+            break
+    return total
+
+def total_timelines(filename):
+    global timeline_grid
+    timeline_grid = [list(row) for row in file_to_array(filename)]
+    start_x = timeline_grid[0].index("S")
+    total = fire_beam_across_time(0, start_x)
+
+    print("Total timelines:", total)
+    return total
+
 # Pt 1
 assert_equals(fire_beams(TEST_INPUT), 21)
 fire_beams(INPUT)
+
+# Pt 1
+assert_equals(total_timelines(TEST_INPUT), 40)
+total_timelines(INPUT)
