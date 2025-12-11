ac72eff6a002dc7b7074f748fde899bb0ecab52c
BearHandedly <bcm3223@gmail.com>
2025-12-11 09:17:35 -0500

    p2 before changes

--- 11/reactor.py
+++ 11/reactor.py
@@ -1,6 +1,7 @@
 from util import assert_equals, file_to_array
 
 TEST_INPUT = "test_input.txt"
+TEST_INPUT_2 = "test_input_2.txt"
 INPUT = "input.txt"
 
 def build_map(filename):
@@ -21,11 +22,36 @@ def total_routes(filename):
                 successful_routes += 1
             else:
                 queue.append(node)
-    print("Total routes:", total_routes)
+    print("Total routes:", successful_routes)
 
     return successful_routes
 
+def svr_routes(filename):
+    connections = build_map(filename)
+    queue = [("svr", set())]
+    successful_routes = 0
+    while len(queue) > 0:
+        current, visited = queue.pop(0)
+        nodes = connections[current]
+
+        for node in nodes:
+            if node == "out":
+                print(visited)
+                if "dac" in visited and "fft" in visited:
+                    successful_routes += 1
+            else:
+                new_visited = visited.copy()
+                new_visited.add(node)
+                queue.append((node, new_visited))
+    print("Total routes:", successful_routes)
+
+    return successful_routes
 
 # Pt 1
 assert_equals(total_routes(TEST_INPUT), 5)
 total_routes(INPUT)
+
+# Pt 1
+assert_equals(svr_routes(TEST_INPUT_2), 2)
+svr_routes(INPUT)
+# bad: 708
