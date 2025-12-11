3e4c955329a6df1e55c2493724e770f3aacc1680
BearHandedly <bcm3223@gmail.com>
2025-12-11 11:27:43 -0500

    @cache

--- 11/reactor.py
+++ 11/reactor.py
@@ -1,3 +1,5 @@
+from functools import cache
+
 from util import assert_equals, file_to_array
 
 TEST_INPUT = "test_input.txt"
@@ -5,12 +7,12 @@ TEST_INPUT_2 = "test_input_2.txt"
 INPUT = "input.txt"
 
 def build_map(filename):
+    global connections
     lines = [line.split(" ") for line in file_to_array(filename)]
     connections = {line[0][:-1]: line[1:] for line in lines}
-    return connections
 
 def total_routes(filename):
-    connections = build_map(filename)
+    build_map(filename)
     queue = ["you"]
     successful_routes = 0
     while len(queue) > 0:
@@ -26,23 +28,27 @@ def total_routes(filename):
 
     return successful_routes
 
-def svr_routes(filename):
-    connections = build_map(filename)
-    queue = [("svr", set())]
-    successful_routes = 0
-    while len(queue) > 0:
-        current, visited = queue.pop(0)
-        nodes = connections[current]
+connections = dict()
 
-        for node in nodes:
-            if node == "out":
-                print(visited)
-                if "dac" in visited and "fft" in visited:
-                    successful_routes += 1
-            else:
-                new_visited = visited.copy()
-                new_visited.add(node)
-                queue.append((node, new_visited))
+@cache
+def find_out(current, visited_dac, visited_fft):
+    successful_paths = 0
+    nodes = connections[current]
+    visited_dac = visited_dac or current == "dac"
+    visited_fft = visited_fft or current == "fft"
+    for node in nodes:
+        if node == "out":
+            if visited_dac and visited_fft:
+                successful_paths += 1
+            continue
+        successful_paths += find_out(node, visited_dac, visited_fft)
+    return successful_paths
+
+def svr_routes(filename):
+    find_out.cache_clear()
+    build_map(filename)
+    successful_routes = find_out("svr", False, False)
+
     print("Total routes:", successful_routes)
 
     return successful_routes
@@ -54,4 +60,3 @@ total_routes(INPUT)
 # Pt 1
 assert_equals(svr_routes(TEST_INPUT_2), 2)
 svr_routes(INPUT)
-# bad: 708
